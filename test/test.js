26409f7ab71d3880d68beac0a51d54e2b325c4f5
Rick Zaki <GitHub@RickZaki.com>
2016-07-30 15:41:42 -0400

    making a change

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var myModule = require('../index');
 describe('Module', function() {
     it('should export "hello world"', function() {
         assert.equal(myModule, 'hello world');
-        // assert.equal(myModule, 'this travis build should fail');
+        assert.equal(myModule, 'this travis build should fail');
     });
 });
