3ee4a7bc847b3e1d6160c82d62b51a5bb53f24ee
Rick Zaki <GitHub@RickZaki.com>
2016-07-30 15:56:18 -0400

    maybe this workflow will work

--- scripts/travis-promote.sh
+++ scripts/travis-promote.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
-if [ "$TRAVIS_BRANCH" != "develop" ]; then
-    printf 'bailing early since not develop\n'
-    exit 0;
-fi
-
-
+BRANCH_TO_MERGE_FROM="develop"
 BRANCH_TO_MERGE_INTO="master"
 GITHUB_REPO="RickZaki/open_npm.git"
 git config --global user.email "travis@RickZaki.com"
 git config --global user.name "Travis CI"
 push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
+
+
+if [ "$TRAVIS_BRANCH" != "$BRANCH_TO_MERGE_FROM" ]; then
+    printf 'bailing early since not %s\n' "$BRANCH_TO_MERGE_FROM" >&2
+    exit 0;
+fi
 
 
 # Since Travis does a partial checkout, we need to get the whole thing
@@ -20,25 +21,22 @@ git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
 # shellcheck disable=SC2164
 cd "$repo_temp"
 
-printf 'Bumping package version\n' >&2
-npm --no-git-tag-version version patch
-git add package.json
-git commit -m "bumping version for release [ci skip]"
-
-printf 'Pushing to %s\n' "$TRAVIS_BRANCH" >&2
-git push "$push_uri" "$TRAVIS_BRANCH" >/dev/null 2>&1
-
 printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
 git checkout "$BRANCH_TO_MERGE_INTO"
 
-printf 'Merging %s\n' "$TRAVIS_BRANCH" >&2
-git merge develop --no-commit
-git commit -m "Merging develop into master"
+printf 'Merging %s\n' "$BRANCH_TO_MERGE_FROM" >&2
+git merge "$BRANCH_TO_MERGE_FROM"
 
-VERSION=`node -e "console.log('v' + require('./package.json').version);"`
-printf 'Tagging %s\n' "$VERSION" >&2
-git tag -a $VERSION -m "tagging for release $VERSION"
+printf 'Bumping package version\n' >&2
+npm version patch
 
+printf 'Pushing to %s\n' "$TRAVIS_BRANCH" >&2
+git push --tags "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
 
-printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
-git push  --tags "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
+printf 'Checking out %s\n' "$BRANCH_TO_MERGE_FROM" >&2
+git checkout "$BRANCH_TO_MERGE_FROM"
+
+printf 'Merging %s\n' "$BRANCH_TO_MERGE_INTO" >&2
+git merge "$BRANCH_TO_MERGE_INTO"  --no-commit
+git commit -m 'Keeping develop in sync with master [ci skip]'
+git push "$push_uri" "$BRANCH_TO_MERGE_FROM" >/dev/null 2>&1
